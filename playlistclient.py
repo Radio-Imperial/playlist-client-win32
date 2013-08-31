e43a0757c3a2e0046905fd0253b235f3340229d8
spiermar <spiermar@gmail.com>
2013-08-31 11:48:30 -0700

    Fixing IOError exception catching bug.

--- playlistclient.py
+++ playlistclient.py
@@ -14,7 +14,7 @@ class PlaylistClientService (win32serviceutil.ServiceFramework):
 	_svc_name_ = "playlist-client"
 	_svc_display_name_ = "playlist-client"
 	
-	def __init__(self,args):
+	def __init__(self, args):
 		win32serviceutil.ServiceFramework.__init__(self,args)
 		self.isAlive = True
 		self.artist = None
@@ -26,7 +26,7 @@ class PlaylistClientService (win32serviceutil.ServiceFramework):
 		config.read(self.path + '\playlistclient.cfg')
 		self.interval = float(config.get('Default', 'interval'))
 		self.playlist_file = config.get('Default', 'playlist_file')
-		# self.playlist_file = config.get('Default', 'loglevel')
+		# self.loglevel = config.get('Default', 'loglevel')
 		logging.basicConfig(filename=self.path + '\playlistclient.log', level=logging.DEBUG)
 
 	def SvcDoRun(self):
@@ -39,22 +39,30 @@ class PlaylistClientService (win32serviceutil.ServiceFramework):
 		self.isAlive = False
 
 	def parse_playlist_xml(self):
+		id3 = None
 		try:
 			tree = ET.parse(self.playlist_file)
 			root = tree.getroot()
 			onair = root.find('OnAir')
 			curins = onair.find('CurIns')
 			id3 = curins.find('ID3')
+		except IOError:
+			logging.error('Cannot read playlist file.')
+			pass
 		except Exception as e:
 			logging.exception(e)
-			logging.error(str(id3))
+			pass
 		if id3 is not None:
-			if ((id3.get('Artist') != self.artist) or (id3.get('Title') != self.title) or (curins.find('Type').text != self.type)):
-				self.artist = id3.get('Artist')
-				self.title = id3.get('Title')
-				self.started_time = curins.find('StartedTime').text
-				self.type = curins.find('Type').text
-				self.update_playlist()
+			try:
+				if ((id3.get('Artist') != self.artist) or (id3.get('Title') != self.title) or (curins.find('Type').text != self.type)):
+					self.artist = id3.get('Artist')
+					self.title = id3.get('Title')
+					self.started_time = curins.find('StartedTime').text
+					self.type = curins.find('Type').text
+					self.update_playlist()
+			except Exception as e:
+				logging.exception(e)
+				pass
 
 	def update_playlist(self):
 		payload = { 'started_time' : self.convert_playlist_time(self.started_time) }
@@ -81,16 +89,22 @@ class PlaylistClientService (win32serviceutil.ServiceFramework):
 			logging.debug('Updating Playlist:')
 			logging.debug(str(payload))
 		except Exception as e:
-			logging.error(e.args[0])
+			logging.exception(e)
+			pass
 
-	def convert_playlist_time(str_time):
-		time = strptime(str_time, '%d/%m/%Y %H:%M:%S')
-		brt = pytz.timezone('America/Sao_Paulo')
-		dt = datetime.fromtimestamp(mktime(time))
-		brt_dt = brt.localize(dt)
-		utc_dt = brt_dt - brt_dt.utcoffset()
-		utc_dt = utc_dt.replace(tzinfo=pytz.utc)
-		timestamp = timegm(utc_dt.utctimetuple())
+	def convert_playlist_time(self, str_time):
+		timestamp = None
+		try:
+			time = strptime(str_time, '%d/%m/%Y %H:%M:%S')
+			brt = pytz.timezone('America/Sao_Paulo')
+			dt = datetime.fromtimestamp(mktime(time))
+			brt_dt = brt.localize(dt)
+			utc_dt = brt_dt - brt_dt.utcoffset()
+			utc_dt = utc_dt.replace(tzinfo=pytz.utc)
+			timestamp = timegm(utc_dt.utctimetuple())
+		except Exception as e:
+			logging.exception(e)
+			pass
 		return timestamp
 
 if __name__ == '__main__':
